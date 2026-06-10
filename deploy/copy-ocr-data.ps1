cb275c3813db567bf5aa1de5cb061c2619529dd7
Ken Watson <furman87@gmail.com>
2026-06-10 17:36:34 -0400

    Limit directories because of a copy hiccup

--- deploy/copy-ocr-data.ps1
+++ deploy/copy-ocr-data.ps1
@@ -1,7 +1,8 @@
 $server = "ken@ns540399.ip-144-217-180.net"
 $localRoot = "D:\Dev\YearbookData\yearbook-data"
 
-Get-ChildItem $localRoot -Directory -Filter "Bonhomie-*" | ForEach-Object {
+Get-ChildItem $localRoot -Directory -Filter "Bonhomie-*" | Where-Object { $_.Name -match '^Bonhomie-199[1-4]$' } |
+ForEach-Object {
     $yearbook = $_.Name
     ssh $server "mkdir -p /opt/yearbook-data/$yearbook"
     scp -r "$($_.FullName)\text" "${server}:/opt/yearbook-data/$yearbook/"
